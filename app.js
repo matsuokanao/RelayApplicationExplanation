47a0cd34465c605fa7ab0bba4b53f3a0ae30d58f
matsuokanao <matsuokanao1029@gmail.com>
2020-08-14 20:48:26 +0900

    heroku

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', function(req, res, next){
+  res.sendStatus(200);
+});
+
+app.get('/index.html', function(req, res,next){
+  res.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.post('/contact', function(req, res, next){
+
+});
+
+
 app.listen(port, () => console.log(`Example app listening at http://localhost: ` + port))
 
 
