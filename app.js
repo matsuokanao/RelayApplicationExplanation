67eb440f432fb514d11feeaff0d28472a5d5fb10
matsuokanao <matsuokanao1029@gmail.com>
2020-08-14 20:43:18 +0900

    heroku

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 8000;
+let server = require('http').Server(app);
+
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost: ` + port))
+
+
+
